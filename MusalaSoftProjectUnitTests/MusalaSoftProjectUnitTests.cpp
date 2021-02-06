5c262d955c282f0346bb2be75bf09f5fb529a7e1
IDIliev18 <IDIliev18@codingburgas.bg>
2021-02-06 20:18:36 +0200

    Add unit tests project files

---
+++ MusalaSoftProjectUnitTests/MusalaSoftProjectUnitTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace MusalaSoftProjectUnitTests
+{
+	TEST_CLASS(MusalaSoftProjectUnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
