73b9f67e2ddeea889837c8f619d825b29d74a7cf
IDIliev18 <IDIliev18@codingburgas.bg>
2021-02-16 08:54:42 +0200

    Define structures
    * Add STUDENT structure
    * Add TEACHER structure
    * Add TEAM structure
    * Add PARTICIPANTS structure
    * Add TEAM_STATUS enum
    * Add ROLES enum
    * Add define.h

---
+++ MusalaSoftProject/define.h
@@ -0,0 +1,54 @@
+#pragma once
+#include <iostream>
+
+constexpr auto MAX_CHAR_ARRAY_LENGTH = 32;
+constexpr auto MAX_ST_CLASS_LENGTH= 32;
+constexpr auto MAX_DESCRIPTION_LENGTH= 256;
+
+enum class TEAM_STATUS
+{
+	IN_USE,
+	NOT_ACTIVE,
+	ARCHIVED
+};
+
+enum class ROLES
+{
+	BACK_END,
+	FRONT_END,
+	SCRUM_MASTER,
+	QA
+};
+
+struct STUDENT
+{
+	int id = 0;
+	char firstName[MAX_CHAR_ARRAY_LENGTH]{};
+	char lastName[MAX_CHAR_ARRAY_LENGTH]{};
+	char studentClass[MAX_ST_CLASS_LENGTH];
+	char email[MAX_CHAR_ARRAY_LENGTH]{};
+};
+
+struct TEACHER
+{
+	int id = 0;
+	char firstName[MAX_CHAR_ARRAY_LENGTH]{};
+	char lastName[MAX_CHAR_ARRAY_LENGTH]{};
+	char email[MAX_CHAR_ARRAY_LENGTH]{};
+};
+
+struct TEAM
+{
+	int id = 0;
+	char teamName[MAX_CHAR_ARRAY_LENGTH]{};
+	char teamDescription[MAX_DESCRIPTION_LENGTH]{};
+	char dateOfSetup[MAX_CHAR_ARRAY_LENGTH]{};
+	TEAM_STATUS teamStatus;
+};
+
+struct PARTICIPANTS
+{
+	int studentId = 0;
+	int teamId = 0;
+	ROLES role;
+};
