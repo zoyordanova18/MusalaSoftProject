cfa8e2af5856a7f6be9e28fab6394f05f88fde8d
srandonova18 <srandonova18@codingburgas.bg>
2021-03-04 00:53:07 +0200

    Add comments to createFilesNeededForTesting()

--- MusalaSoftProject-Unit-tests/HelperFunctionsForTesting.cpp
+++ MusalaSoftProject-Unit-tests/HelperFunctionsForTesting.cpp
@@ -5,8 +5,11 @@ using namespace std;
 
 void createFilesNeededForTesting(const char* fileName)
 {
+	// Remove old file
 	remove(fileName);
 	bool isOpened = STUDENT_SERVICE::open(fileName);
+
+	// Initialize some "students" for testing
 	STUDENT_SERVICE::add({ 1, "Maksim", "Dimitrov", "10V", "MDDimitrov@codingburgas.bg" });
 	STUDENT_SERVICE::add({ 2, "Pedro", "Geshev", "10A", "PMGeshev@codingburgas.bg" });
 	STUDENT_SERVICE::add({ 3, "Vanq", "Velikova", "10B", "VVVelikova@codingburgas.bg" });
