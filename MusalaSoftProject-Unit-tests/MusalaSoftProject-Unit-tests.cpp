b23e9b1bf84766ae6938f3e0ec631ab39ceb0516
srandonova18 <srandonova18@codingburgas.bg>
2021-03-03 12:42:38 +0200

    Configure and add files for unit testing

---
+++ MusalaSoftProject-Unit-tests/MusalaSoftProject-Unit-tests.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../MusalaSoftProject/students.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace MusalaSoftProjectUnittests
+{
+	TEST_CLASS(MusalaSoftProjectUnittests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
