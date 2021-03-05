0253cdf9a330d6a0863e364b1e931e58e4c99202
IDIliev18 <IDIliev18@codingburgas.bg>
2021-03-05 14:19:16 +0200

    Add PARTICIPANT_SERVICE::open()

--- MusalaSoftProject/participants.cpp
+++ MusalaSoftProject/participants.cpp
@@ -1,1 +1,22 @@
 #include "participants.h"
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+fstream participantsFile;
+
+bool PARTICIPANT_SERVICE::open(const char* fileName)
+{
+	/*studentsFile.open(fileName, ios::in | ios::out | ios::_Noreplace);
+
+	if (studentsFile.is_open() == false)
+	{
+		studentsFile.open(fileName, ios::out);
+		studentsFile.close();
+	}*/
+
+	participantsFile.open(fileName, ios::ate | ios::binary | ios::in | ios::out);
+	return participantsFile.is_open();
+}
--- MusalaSoftProject/participants.h
+++ MusalaSoftProject/participants.h
@@ -1,4 +1,8 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+
+extern std::fstream participantsFile;
 
 enum class ROLES
 {
@@ -8,10 +12,15 @@ enum class ROLES
 	QA
 };
 
-struct PARTICIPANTS
+struct PARTICIPANT
 {
 	int studentId;
 	int teamId;
 	ROLES role;
+};
+
+struct PARTICIPANT_SERVICE
+{
+	static bool open(const char* fileName = "participants.txt");
 };
 
