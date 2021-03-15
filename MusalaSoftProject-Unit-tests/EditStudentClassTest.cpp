ed3b481121957332087f487f0fd75a5546ce12e9
srandonova18 <srandonova18@codingburgas.bg>
2021-03-15 22:58:33 +0200

    Fix EditStudentClassTets with optimized open()

--- MusalaSoftProject-Unit-tests/EditStudentClassTest.cpp
+++ MusalaSoftProject-Unit-tests/EditStudentClassTest.cpp
@@ -10,20 +10,20 @@ namespace MusalaSoftProjectUnitTests
 	TEST_CLASS(editStudentClassTest)
 	{
 	public:
-
 		TEST_METHOD(shouldEditStudentClass)
 		{
-			/*// Arrange
-			createFilesNeededForTesting("students_editClassTest.txt");
+			// Arrange
+			createFilesNeededForTestingStudent("students_editStudentClassTest.txt");
 
 			// Act
 			STUDENT_SERVICE::editClass(2, "10G");
 			std::vector<STUDENT> students = STUDENT_SERVICE::getAll();
 			STUDENT student = findStudentById(students, 2);
 			STUDENT_SERVICE::close();
+			remove("students_editStudentClassTest.txt");
 
 			// Assert
-			Assert::AreEqual(student.studentClass, "10G");*/
+			Assert::AreEqual(student.studentClass, "10G");
 		}
 	};
 }
